f4fdd53f7dbe6e03bffb1a3c26b8292f5c700cff
johanneshovi-arch <johannes.hovi@gmail.com>
2026-04-16 18:13:34 +0300

    landin page

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import LandingPage from './LandingPage.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import LandingPage from "./LandingPage.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* More specific route first */}
         <Route path="/landing" element={<LandingPage />} />
+        {/* "/" and all in-app paths (/sources, /copilot, /settings, …) */}
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
